99a0fc273500136f2b2f10a07af936ed275d481f
AlexPr06 <freezay00828002@gmail.com>
2025-11-08 00:40:40 +0200

    1. Logger class created.

--- Logger/Program.cs
+++ Logger/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Logger.WriteLog("First log is hear!");
+
+
+class Logger
+{
+    static readonly string s_logsPath;
+    // it is unable to create struct type of const
+    readonly DateTime time;
+    static Logger() {
+        string projectDir = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\..\"));
+        s_logsPath = Path.Combine(projectDir, "LogsFolder", "logs.txt");
+    }
+
+    public Logger()
+    {
+        time = DateTime.Now;
+    }
+
+    static public void WriteLog(string log)
+    {
+        try
+        {
+            using var writer = new StreamWriter(s_logsPath, append: true);
+            writer.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} | {log}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[Logger Error] {ex.Message}");
+        }
+    }
+}
